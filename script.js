93bca52e12b30c8f54101efb690b4b4f375b477c
Mujahid <mujahidrakibdev@gmail.com>
2025-03-05 07:40:28 +0600

    almost complete

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const progress = document.querySelector(".progress")
 const prevBtn = document.querySelector(".prev")
 const nextBtn = document.querySelector(".next")
 const submitBtn = document.querySelector(".submit")
+const steps = document.querySelectorAll(".step")
+const stepsContainer = document.querySelector(".step-container")
+
 
 
 
@@ -12,9 +15,16 @@ function updateProgress() {
     let width = currStep / (stepIndicator.length - 1)
     progress.style.transform = `scaleX(${width})`
 
+    stepsContainer.style.height = steps[currStep].offsetHeight + "px"
+
     stepIndicator.forEach((indicator, index) => {
         indicator.classList.toggle("current", currStep === index)
         indicator.classList.toggle("done", currStep > index)
+    })
+
+    steps.forEach((step, index) => {
+        step.style.transform = `translateX(-${currStep * 100}%)`
+        step.classList.toggle("current", currStep === index)
     })
 
     updateButton()
@@ -24,6 +34,11 @@ function updateButton() {
     prevBtn.hidden = currStep === 0
     nextBtn.hidden = currStep >= stepIndicator.length - 1
     submitBtn.hidden = !nextBtn.hidden
+}
+
+const validStep = ()=> {
+    const fields = steps[currStep].querySelectorAll("input, textarea")
+    return [...fields].every((field) => field.reportValidity())
 }
 
 prevBtn.addEventListener("click", function (e) {
@@ -37,6 +52,8 @@ prevBtn.addEventListener("click", function (e) {
 
 nextBtn.addEventListener("click", function (e) {
     e.preventDefault()
+
+    if(!validStep()) return;
 
     if (currStep < stepIndicator.length - 1) {
         currStep++
